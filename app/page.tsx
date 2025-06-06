94735632f0e9c116511ad284853b3766800cb41b
Amos Mutai <108726397+MutaiOfficial01@users.noreply.github.com>
2025-06-06 21:59:21 +0300

    saas

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,78 @@
+export const dynamic = 'force-dynamic';
 
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
-import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.action";
-import {getSubjectColor} from "@/lib/utils";
+import { getAllCompanions, getRecentSessions } from "@/lib/actions/companion.action";
+import { getSubjectColor } from "@/lib/utils";
 
 const Page = async () => {
-    const companions = await getAllCompanions({ limit: 3});
+    const companions = await getAllCompanions({ limit: 3 });
     const recentSessionsCompanions = await getRecentSessions(10);
-  return (
-    <main>
-      <h1 className="text-2xl underline">Popular Companions</h1>
-      <section className="home-section">
-          {companions?.map((companion) => (
-              <CompanionCard
-                  key={companion.id}
-                  {...companion}
-                  color={getSubjectColor(companion.subject)}
-              />
-          ))}
+    return (
+        <main>
+            <h1 className="text-2xl underline">Popular Companions</h1>
+            <section className="home-section">
+                {companions?.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently completed sessions"
+                    companions={recentSessionsCompanions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <CTA />
+            </section>
+        </main>
+    );
+};
 
-      </section>
-        <section className="home-section">
-            <CompanionsList
-            title="Recently completed sessions"
-            companions={recentSessionsCompanions}
-            classNames="w-2/3 max-lg:w-full"
-            />
-            <CTA />
-        </section>
-    </main>
-  )
-}
+export default Page;
 
-export default Page
+
+
+//
+// import CompanionCard from "@/components/CompanionCard";
+// import CompanionsList from "@/components/CompanionsList";
+// import CTA from "@/components/CTA";
+// import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.action";
+// import {getSubjectColor} from "@/lib/utils";
+//
+// const Page = async () => {
+//     const companions = await getAllCompanions({ limit: 3});
+//     const recentSessionsCompanions = await getRecentSessions(10);
+//   return (
+//     <main>
+//       <h1 className="text-2xl underline">Popular Companions</h1>
+//       <section className="home-section">
+//           {companions?.map((companion) => (
+//               <CompanionCard
+//                   key={companion.id}
+//                   {...companion}
+//                   color={getSubjectColor(companion.subject)}
+//               />
+//           ))}
+//
+//       </section>
+//         <section className="home-section">
+//             <CompanionsList
+//             title="Recently completed sessions"
+//             companions={recentSessionsCompanions}
+//             classNames="w-2/3 max-lg:w-full"
+//             />
+//             <CTA />
+//         </section>
+//     </main>
+//   )
+// }
+//
+// export default Page
 
 // <CompanionCard
 // id="456"
